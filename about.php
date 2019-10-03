f9a90bdc5cf6dcbfa0ca54a1050efd69943eafdc
Jishan Hoshen <70538593+jishanhoshen@users.noreply.github.com>
2019-10-04 01:42:46 +0600

    b13

---
+++ about.php
@@ -0,0 +1,79 @@
+<!DOCTYPE HTML>
+<html>
+	<head>
+  <title>B Thirteen | Organisation</title>
+  	<meta charset="UTF-8">
+	<meta name="title" content="B Thirteen | Organisation ">
+	<meta name="description" content="B Thirteen is an Organisation where have different types of services for people. such as savings, money transfer, new product auctions etc.|বি থার্টিন একটি সংস্থা যেখানে মানুষের জন্য বিভিন্ন ধরণের পরিষেবা আছে। যেমন সঞ্চয়, অর্থ স্থানান্তর, নতুন পণ্য নিলাম ইত্যাদি।">
+	<meta name="keywords" content="b thirteen, b13, bthirteen, bid, money, money transfer, product auctions, savings">
+	<meta name="robots" content="index, follow">
+	<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+	<meta name="language" content="English">
+	<meta name="revisit-after" content="1 days">
+	<meta name="author" content="Sariful Islam">
+  <link href="https://fonts.googleapis.com/css?family=Open+Sans:400,700" rel="stylesheet">
+		<link rel="stylesheet" href="assets/css/main.css" />
+		<link rel="stylesheet" href="assets/css/style.css" />
+	</head>
+	<body>
+
+		<!-- Header -->
+			<header id="header" class="hide">
+				<div class="inner">
+					<div class="content">
+						<h1>B Thirteen</h1>
+						<h2>Welcome To<br />
+						Your Dream Land</h2>
+						<a href="#" class="button big alt"><span>Let's Go</span></a>
+					</div>
+					<a href="https://bthirteen.org" class="button hidden"><span>Let's Go</span></a>
+				</div>
+			</header>
+		<!-- Main -->
+			<div id="main">
+			    <div class="navbar">
+			        <div class="logo"><h1>B thirteen</h1></div>
+			        <div class="menu">
+			            <ul>
+			                <li><a href="https://bthirteen.org">Home</a></li>
+			                <li><a href="about.php">About</a></li>
+			                <li><a href="services.php">Services</a></li>
+			                <li><a href="portfolio.php">Portfolio</a></li>
+			                <li><a href="contact.php">Contact Us</a></li>
+			            </ul>
+			        </div>
+			    </div>
+			    <div class="section" id="home">
+			        <div class="content"><h1>About</h1>
+			        </div>
+			    </div>
+			</div>
+		<!-- Footer -->
+			<footer id="footer">
+				<a href="#" class="info fa fa-info-circle"><span>About</span></a>
+				<div class="inner">
+					<div class="content">
+						<h3>Vestibulum hendrerit tortor id gravida</h3>
+						<p>In tempor porttitor nisl non elementum. Nulla ipsum ipsum, feugiat vitae vehicula vitae, imperdiet sed risus. Fusce sed dictum neque, id auctor felis. Praesent luctus sagittis viverra. Nulla erat nibh, fermentum quis enim ac, ultrices euismod augue. Proin ligula nibh, pretium at enim eget, tempor feugiat nulla.</p>
+					</div>
+					<div class="copyright">
+						<h3>Follow me</h3>
+						<ul class="icons">
+							<li><a href="#" class="icon fa-twitter"><span class="label">Twitter</span></a></li>
+							<li><a href="#" class="icon fa-facebook"><span class="label">Facebook</span></a></li>
+							<li><a href="#" class="icon fa-instagram"><span class="label">Instagram</span></a></li>
+							<li><a href="#" class="icon fa-dribbble"><span class="label">Dribbble</span></a></li>
+						</ul>
+						&copy; B Thirteen developers | 2019
+					</div>
+				</div>
+			</footer>
+
+		<!-- Scripts -->
+			<script src="assets/js/jquery.min.js"></script>
+			<script src="assets/js/skel.min.js"></script>
+			<script src="assets/js/util.js"></script>
+			<script src="assets/js/main.js"></script>
+
+	</body>
+</html>
---
+++ services.php
@@ -0,0 +1,181 @@
+<!DOCTYPE HTML>
+<html>
+	<head>
+  <title>B Thirteen | Organisation</title>
+  	<meta charset="UTF-8">
+	<meta name="title" content="B Thirteen | Organisation ">
+	<meta name="description" content="B Thirteen is an Organisation where have different types of services for people. such as savings, money transfer, new product auctions etc.|বি থার্টিন একটি সংস্থা যেখানে মানুষের জন্য বিভিন্ন ধরণের পরিষেবা আছে। যেমন সঞ্চয়, অর্থ স্থানান্তর, নতুন পণ্য নিলাম ইত্যাদি।">
+	<meta name="keywords" content="b thirteen, b13, bthirteen, bid, money, money transfer, product auctions, savings">
+	<meta name="robots" content="index, follow">
+	<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+	<meta name="language" content="English">
+	<meta name="revisit-after" content="1 days">
+	<meta name="author" content="Sariful Islam">
+  <link href="https://fonts.googleapis.com/css?family=Open+Sans:400,700" rel="stylesheet">
+		<link rel="stylesheet" href="assets/css/main.css" />
+		<link rel="stylesheet" href="assets/css/style.css" />
+	</head>
+	<body>
+
+		<!-- Header -->
+			<header id="header" class="hide">
+				<div class="inner">
+					<div class="content">
+						<h1>B Thirteen</h1>
+						<h2>Welcome To<br />
+						Your Dream Land</h2>
+						<a href="#" class="button big alt"><span>Let's Go</span></a>
+					</div>
+					<a href="https://bthirteen.org" class="button hidden"><span>Let's Go</span></a>
+				</div>
+			</header>
+		<!-- Main -->
+			<div id="main">
+			    <div class="navbar">
+			        <div class="logo"><h1>B thirteen</h1></div>
+			        <div class="menu">
+			            <ul>
+			                <li><a href="https://bthirteen.org">Home</a></li>
+			                <li><a href="about.php">About</a></li>
+			                <li><a href="services.php">Services</a></li>
+			                <li><a href="portfolio.php">Portfolio</a></li>
+			                <li><a href="contact.php">Contact Us</a></li>
+			            </ul>
+			        </div>
+			    </div>
+			    <div class="section" id="home">
+			        <div class="content"><h1>Services</h1>
+
+
+<div class="tab">
+  <button class="tablinks" onclick="openCity(event, 'services')">Services</button>
+  <button class="tablinks" onclick="openCity(event, 'London')">Cheap Plan</button>
+  <button class="tablinks" onclick="openCity(event, 'Paris')">Business class</button>
+  <button class="tablinks" onclick="openCity(event, 'Tokyo')">Offers</button>
+</div>
+<!-- Tab content -->
+<div id="services" class="tabcontent">
+  <h3>Services</h3>
+  <div class="serv_box">
+      <h4>We Make Your Dream Site</h4>
+      <img src="images/icons/website.png"/>
+      <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,</p>
+  </div>
+  <div class="serv_box">
+      <h4>We Make Your Dream Site</h4>
+      <img src="images/icons/website.png"/>
+      <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,</p>
+  </div>
+  <div class="serv_box">
+      <h4>We Make Your Dream Site</h4>
+      <img src="images/icons/website.png"/>
+      <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,</p>
+  </div>
+</div>
+<div id="London" class="tabcontent">
+  <h3>Cheap Plan</h3>
+  <div class="serv_box">
+      <ul>
+          <li><h3>Poor lite Package</h3></li>
+          <li>1 Time 555tk</li>
+          <li>Annualy free</li>
+          <li>1 Subomain</li>
+          <li>1 WebSite</li>
+          <li>Profile/Bio/Portfolio</li>
+          <li>No Cpanel</li>
+          <li>---</li>
+          <li><button>Order Now</button></li>
+      </ul>
+  </div>
+  <div class="serv_box">
+      <ul>
+          <li><h3>Good lite Package</h3></li>
+          <li>1st Year 2000tk</li>
+          <li>Annually 1500tk</li>
+          <li>.com Domain</li>
+          <li>1 WebSite</li>
+          <li>Profile/Bio/Portfolio</li>
+          <li>No Cpanel</li>
+          <li>---</li>
+          <li><button>Order Now</button></li>
+      </ul>
+  </div>
+  <div class="serv_box">
+      <ul>
+          <li><h3>Best lite Package</h3></li>
+          <li>1st Year 3500tk</li>
+          <li>Annually 3000tk</li>
+          <li>.com Domain</li>
+          <li>1 WebSite</li>
+          <li>Profile/Bio/Portfolio</li>
+          <li>1 Cpanel</li>
+          <li>---</li>
+          <li><button>Order Now</button></li>
+      </ul>
+  </div>
+</div>
+
+<div id="Paris" class="tabcontent">
+  <h3>Paris</h3>
+  <p>Paris is the capital of France.</p> 
+</div>
+
+<div id="Tokyo" class="tabcontent">
+  <h3>Tokyo</h3>
+  <p>Tokyo is the capital of Japan.</p>
+</div>
+<!-- Script for tab -->
+<script>
+function openCity(evt, cityName) {
+  // Declare all variables
+  var i, tabcontent, tablinks;
+
+  // Get all elements with class="tabcontent" and hide them
+  tabcontent = document.getElementsByClassName("tabcontent");
+  for (i = 0; i < tabcontent.length; i++) {
+    tabcontent[i].style.display = "none";
+  }
+
+  // Get all elements with class="tablinks" and remove the class "active"
+  tablinks = document.getElementsByClassName("tablinks");
+  for (i = 0; i < tablinks.length; i++) {
+    tablinks[i].className = tablinks[i].className.replace(" active", "");
+  }
+
+  // Show the current tab, and add an "active" class to the button that opened the tab
+  document.getElementById(cityName).style.display = "block";
+  evt.currentTarget.className += " active";
+}
+</script>
+			        </div>
+			    </div>
+			</div>
+		<!-- Footer -->
+			<footer id="footer">
+				<a href="#" class="info fa fa-info-circle"><span>About</span></a>
+				<div class="inner">
+					<div class="content">
+						<h3>Vestibulum hendrerit tortor id gravida</h3>
+						<p>In tempor porttitor nisl non elementum. Nulla ipsum ipsum, feugiat vitae vehicula vitae, imperdiet sed risus. Fusce sed dictum neque, id auctor felis. Praesent luctus sagittis viverra. Nulla erat nibh, fermentum quis enim ac, ultrices euismod augue. Proin ligula nibh, pretium at enim eget, tempor feugiat nulla.</p>
+					</div>
+					<div class="copyright">
+						<h3>Follow me</h3>
+						<ul class="icons">
+							<li><a href="#" class="icon fa-twitter"><span class="label">Twitter</span></a></li>
+							<li><a href="#" class="icon fa-facebook"><span class="label">Facebook</span></a></li>
+							<li><a href="#" class="icon fa-instagram"><span class="label">Instagram</span></a></li>
+							<li><a href="#" class="icon fa-dribbble"><span class="label">Dribbble</span></a></li>
+						</ul>
+						&copy; B Thirteen developers | 2019
+					</div>
+				</div>
+			</footer>
+
+		<!-- Scripts -->
+			<script src="assets/js/jquery.min.js"></script>
+			<script src="assets/js/skel.min.js"></script>
+			<script src="assets/js/util.js"></script>
+			<script src="assets/js/main.js"></script>
+
+	</body>
+</html>
